a60ff33cc575c4e83dad06999e7c571d861e5260
Mauro <mrb030584@yahoo.com>
2021-03-16 19:03:58 -0300

    Tipos de parametros

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
+const { request, json } = require('express');
 const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/courses", (req, resp) => {
+    const query = req.query;
+    console.log(query);
     return resp.json(['curso1', 'curso2', 'curso3']);
 });
 
 app.post("/courses", (req, resp) => {
+    const body = req.body;
+    console.log(body);
     return resp.json(['curso1', 'curso2', 'curso3', 'curso4']);
 });
 
 app.put("/courses/:id", (req, resp) => {
-    
+    const  { id }= req.params;
+    console.log(id);
     return resp.json(['curso6', 'curso2', 'curso3', 'curso4']);
 });
 
