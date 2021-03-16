878c35e296bf581c9b3c26cd500535d3ea495a97
Mauro <mrb030584@yahoo.com>
2021-03-16 14:07:34 -0300

    Adcionado o Nodemon na aplicação

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, resp) => {
-    return resp.json({message: "Hello World"});
+    return resp.json({message: "Hello World Ignite"});
 });
 
 app.listen(3333);
