42595c6ebb343d68a97cb40d8a6884821db57db7
Mauro <mrb030584@yahoo.com>
2021-03-16 14:58:59 -0300

    Utilizando os métodos HTPP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, resp) => {
-    return resp.json({message: "Hello World Ignite"});
+app.get("/courses", (req, resp) => {
+    return resp.json(['curso1', 'curso2', 'curso3']);
+});
+
+app.post("/courses", (req, resp) => {
+    return resp.json(['curso1', 'curso2', 'curso3', 'curso4']);
+});
+
+app.put("/courses/:id", (req, resp) => {
+    
+    return resp.json(['curso6', 'curso2', 'curso3', 'curso4']);
+});
+
+app.patch("/courses/:id", (req, resp) => {
+    
+    return resp.json(['curso6', 'curso7', 'curso3', 'curso4']);
+});
+
+app.delete("/courses/:id", (req, resp) => {
+    
+    return resp.json(['curso6', 'curso7',  'curso4']);
 });
 
 app.listen(3333);
